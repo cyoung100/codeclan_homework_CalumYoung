6a94506946f57b500cb7b759d0d3ac4acf611673
cyoung100 <137174812+cyoung100@users.noreply.github.com>
2023-08-29 18:08:45 +0100

    eidt

--- week_05/day_05/Sharks app/shark_attacks_app.R
+++ week_05/day_05/Sharks app/shark_attacks_app.R
@@ -1,7 +1,6 @@
 library(shiny)
 library(ggplot2)
 library(DT)
-
 data <- shark
 
 ui <- fluidPage(
@@ -12,7 +11,6 @@ ui <- fluidPage(
              sidebarLayout(
                sidebarPanel(
                  selectInput("variable1", "Select Variable 1:", choices = names(data)),
-                 selectInput("year", "Country", choices = shark$Country),
                  sliderInput("year_range", "Select Year Range:",
                              min = min(data$Year), max = max(data$Year),
                              value = c(min(data$Year), max(data$Year)),
@@ -42,9 +40,9 @@ server <- function(input, output) {
   
   output$count_plot <- renderPlot({
     var1_counts <- filtered_data() %>%
-      count(.data[[input$variable1]], sort = TRUE)
+      count(!!sym(input$variable1), sort = TRUE)
     
-    p <- ggplot(var1_counts, aes(x = .data[[input$variable1]], y = n)) +
+    p <- ggplot(var1_counts, aes_string(x = input$variable1, y = "n")) +
       geom_bar(stat = "identity", fill = "steelblue") +
       labs(x = input$variable1, y = "Count", title = "Attacks by variable") +
       coord_flip() +
@@ -54,10 +52,9 @@ server <- function(input, output) {
   })
   
   output$data_table_info <- renderDT({
-    datatable(data,
-              options = list(pageLength = 5))
+    datatable(filtered_data(),
+              options = list(pageLength = 12))
   })
 }
 
 shinyApp(ui = ui, server = server)
-
